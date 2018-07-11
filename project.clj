9e2b2d4e5e2866f54211e9e37e3db321ddf6945f
Josef Erben <josef@erben.io>
2018-07-11 18:51:33 +0200

    Remove cheshire, dump temporary results into atom

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.8.0"]
                  [metosin/jsonista "0.2.1"]]
   :main ^:skip-aot ndjson2csv.core
   :target-path "target/%s"
--- src/ndjson2csv/core.clj
+++ src/ndjson2csv/core.clj
@@ -1,8 +1,9 @@
 (ns ndjson2csv.core
   (:require
-   [jsonista.core :as j]
-   [cheshire.core :refer [parse-string]])
+   [jsonista.core :as j])
   (:gen-class))
+
+(def memory (atom {}))
 
 (defn deep-merge [v & vs]
   (letfn [(rec-merge [v1 v2]
@@ -13,34 +14,17 @@
       (reduce #(rec-merge %1 %2) v vs)
       v)))
 
-(defn merge-documents
-  [docs]
-  (loop [to-merge docs
-         subjects {}
-         n 0]
-    (if (empty? to-merge)
-      subjects
-      (let [current-result (first to-merge)]
-        ;;(prn (str "progress: " n "/" (count docs)))
-        ;;(prn (get-in current-result ["subject" "$oid"]))
-        (recur (rest to-merge)
-               (update subjects
-                       (get-in current-result ["subject" "$oid"])
-                       deep-merge
-                       current-result)
-               (inc n))))))
-
-(defn dnjson2csv
-  [results]
-  (let [partition-size (int (/ (count results) 4))]
-     (->> (map j/read-value results)
-          (partition partition-size)
-          (map merge-documents)
-          (reduce deep-merge))))
+(defn merge-document
+  [subjects doc]
+  (update subjects
+          (get-in doc ["subject" "$oid"])
+          deep-merge
+          doc))
 
 (defn -main
   [& args]
-  (prn "Start reading file")
   (with-open [rdr (clojure.java.io/reader (clojure.java.io/resource "results"))]
-    (let [results (take 10000 (line-seq rdr))]
-      (prn (count (dnjson2csv results))))))
+    (let [results (line-seq rdr)]
+      (doseq [result results]
+        (swap! memory merge-document (j/read-value result)))))
+  (prn (count (keys @memory))))
