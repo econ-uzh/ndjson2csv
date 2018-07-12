8bb4991bbbe4f860f91d39106839d65b2289904d
Josef Erben <josef@erben.io>
2018-07-12 17:12:56 +0200

    Add command line parser, enable command line usage

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
-                 [metosin/jsonista "0.2.1"]]
+                 [metosin/jsonista "0.2.1"]
+                 [org.clojure/tools.cli "0.3.1"]]
   :main ^:skip-aot ndjson2csv.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ndjson2csv/core.clj
+++ src/ndjson2csv/core.clj
@@ -1,13 +1,12 @@
 (ns ndjson2csv.core
   (:require
    [jsonista.core :as j]
-   [clojure.data.csv :as csv])
+   [clojure.data.csv :as csv]
+   [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-;; Deep-merging state in memory line-by-line
-(def memory (atom {}))
-
 (defn deep-merge [v & vs]
+  "Deeply merges multiple maps."
   (letfn [(rec-merge [v1 v2]
             (if (and (map? v1) (map? v2))
               (merge-with deep-merge v1 v2)
@@ -17,13 +16,14 @@
       v)))
 
 (defn merge-document
-  [subjects doc]
-  (update subjects
+  [acc doc]
+  (update acc
           (get doc "subjectId")
           deep-merge
           doc))
 
 (defn flatten-map
+  "Flattens a map in a performant way using a `separator`, doesn't flat vectors."
   ([form separator]
    (into {} (flatten-map form separator nil)))
   ([form separator pre]
@@ -34,39 +34,62 @@
                  [[(keyword prefix) v]])))
            form)))
 
-(defn compare-fields
-  [a b]
-  (cond
-    (and (clojure.string/includes? (name a) "data")
-         (clojure.string/includes? (name b) "data"))
-    0
-    (clojure.string/includes? (name a) "data") 1
-    :else -1))
-
 (defn extract-keys
-  [subjects]
-  (->> (reduce (fn [all-keys subject]
-                 (into #{} (concat all-keys (keys subject))))
-               #{}
-               subjects)
-       (sort compare-fields)))
+  "Given a list of maps `acc`, returns a set of all keys found."
+  [acc]
+  (reduce (fn [all-keys item]
+            (into #{} (concat all-keys (keys item))))
+          #{}
+          acc))
 
 (defn map->cells [fields m]
-  (vec (map (fn [k] (get m k "")) fields)))
+  (map (fn [k] (get m k "")) fields))
 
-(defn write-csv! [fields maps]
+(defn write-csv!
+  "Given flattened `maps` and a list of csv `fields`, writes the result to file."
+  [fields maps file-name]
+  (println (str "Writing csv file"))
   (let [cells (vec (map (partial map->cells fields) maps))]
-    (with-open [w (clojure.java.io/writer "results.csv")]
+    (with-open [w (clojure.java.io/writer file-name)]
       (csv/write-csv w
            (concat (conj [] (vec (map name fields))) cells))))
-  (prn (str "Lines written: " (count maps))))
+  (println (str "Lines written: " (count maps))))
+
+(defn ndjson->map [lines]
+  (let [memory (atom {})]
+    (println (str "Processing ndjson lines"))
+    (doseq [line lines]
+      ;; load lines one by one into memory and process them
+      (swap! memory merge-document (j/read-value line)))
+    (swap! memory vals)
+    (swap! memory (fn [item] (map #(flatten-map % ".") item)))
+    @memory))
+
+(def cli-options
+  [["-l" "--lines NUMBER_LINES" "Number of lines to read from ndjson, can be used for testing"
+    :id :lines
+    :default 0
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 %) "Must be positive"]]
+   ["-i" "--input FILE" "Input file containing ndjson"
+    :validate [(complement clojure.string/blank?) "Must not be empty"]]
+   ["-o" "--output FILE" "File to write csv in, will be created if it doesn't exist"
+    :validate [(complement clojure.string/blank?) "Must not be empty"]
+    :default "results.csv"]
+   ["-h" "--help"
+    :default false]])
 
 (defn -main
   [& args]
-  (with-open [rdr (clojure.java.io/reader (clojure.java.io/resource "results"))]
-    (let [results (line-seq rdr)]
-      (doseq [result results]
-        (swap! memory merge-document (j/read-value result)))))
-  (swap! memory vals)
-  (swap! memory (fn [subjects] (map #(flatten-map % ".") subjects)))
-  (write-csv! (extract-keys @memory) @memory))
+  (let [{:keys [options arguments summary errors]}
+        (parse-opts args cli-options)]
+    (cond
+      (not-empty errors) (println errors)
+      (:help options) (println summary)
+      (nil? (:input options)) (-main "-h")
+      :else
+      (with-open [rdr (clojure.java.io/reader (:input options))]
+        (let [acc (ndjson->map (if (zero? (:lines options))
+                                 (line-seq rdr)
+                                 (take (:lines options) (line-seq rdr))))]
+          (write-csv! (extract-keys acc) acc (:output options)))))))
