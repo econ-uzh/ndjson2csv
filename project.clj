ea5d1ed45828ffdb5825d7b351bf0d68d6448fd1
Josef Erben <josef.erben@uzh.ch>
2018-07-12 17:38:17 +0200

    Setup lein bin

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject ndjson2csv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+(defproject ndjson2csv "0.1.0"
+  :description "NDJSON to CSV converter"
+  :url "https://econ.uzh.ch"
+  :license {:name "Proprietary"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
@@ -9,4 +9,6 @@
                  [org.clojure/tools.cli "0.3.1"]]
   :main ^:skip-aot ndjson2csv.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-binplus "0.6.4"]]}}
+  :bin {:name "ndjson2csv"})
